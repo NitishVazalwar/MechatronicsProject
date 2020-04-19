da73071704758374b617c53353f2b27610d22e46
KaiserS0ze <abhijeet.ds63@gmail.com>
2020-04-18 18:45:04 -0600

    Added comments and functions

--- one.c
+++ one.c
@@ -2,26 +2,39 @@
 #include <util/delay.h>
 #include<stdio.h>
 #include "delay.h" 
-
+#include <stdint.h>
 
 #define BLINK_DELAY_MS 5000
+//Delay function
+void Delay_function(uint16_t dly)
+{
+	for(volatile uint16_t i=dly;i<=0;i--)
+	{
+	;	
+	}
+}
 
+//pin set to output function
 void set_to_output()
 {
 	DDRB |= _BV(DDB5);
 }
 
+//function to turn led on
 void led_is_on()
 {
 	 PORTB |= _BV(PORTB5); 
 }
 
-
+//function to turn led off
 void led_is_off()
 {
 	 PORTB &= ~_BV(PORTB5);
 }
  
+
+//Function to convert decimal to binary 
+
 int main (void)
 {
 
@@ -35,12 +48,12 @@ printf("\n\rLed is on now");
   /* set pin 5 high to turn led on */
  led_is_on();
   _delay_ms(BLINK_DELAY_MS);
-//	Delay(5000);
+	//Delay_function(100);
  printf("\n\rLed is off now");
   /* set pin 5 low to turn led off */
  led_is_off();
   _delay_ms(BLINK_DELAY_MS);
-//	Delay(5000);
+	//Delay_function(100);
  }
  return 0;
 }
